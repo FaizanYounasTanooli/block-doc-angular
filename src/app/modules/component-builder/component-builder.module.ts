6aadb6ad4a3a400170eaaa9114b1d66862736c86
Faizan Younas Tanooli <faizan.tanooli@swipbox.com>
2024-01-23 18:01:26 +0500

    Module Import fix

--- src/app/modules/component-builder/component-builder.module.ts
+++ src/app/modules/component-builder/component-builder.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DesignerComponent } from './designer/designer.component';
 import { ComponentContainerComponent } from './designer/component-container/component-container.component';
-import { TextComponent } from './base-components/text/text.component';
-import { ListComponent } from './base-components/list/list.component';
+import { BaseComponentsModule } from './base-components/base-components.module';
 
 
 @NgModule({
-  declarations: [DesignerComponent,ComponentContainerComponent,TextComponent,ListComponent],
+  declarations: [DesignerComponent,ComponentContainerComponent],
   imports: [
     CommonModule,
+    BaseComponentsModule,
   ],
   exports:[
     DesignerComponent,
